9722e3f0e976099ca482a7501043c48a523a6da7
rooot <hey@rooot.gay>
2024-02-07 19:00:48 +0100

    feat: start working on initial SFH syncing
    Signed-off-by: rooot <hey@rooot.gay>

--- src/mirror.rs
+++ src/mirror.rs
@@ -23,7 +23,30 @@ pub(crate) async fn check_initial_sync(pool: Pool<Sqlite>) -> anyhow::Result<boo
 pub(crate) async fn initial_sync(pool: Pool<Sqlite>) -> anyhow::Result<()> {
     let mut conn = pool.acquire().await?;
 
-    // todo
+    let reqwest = reqwest::Client::new();
+    let response = reqwest
+        .get("https://api.songfilehub.com/songs")
+        .send()
+        .await?
+        .json::<Vec<crate::types::Song>>()
+        .await?;
+
+    for song in response {
+        println!("inserting song: {}", song.id);
+        sqlx::query!(
+            "INSERT INTO songs (id, external_url, name, song_name, song_id, download_url, level_id)
+                VALUES (?, ?, ?, ?, ?, ?, ?)",
+            song.id,
+            song.external_url,
+            song.name,
+            song.song_name,
+            song.song_id,
+            song.download_url,
+            song.level_id,
+        )
+        .execute(&mut *conn)
+        .await?;
+    }
 
     sqlx::query!("INSERT INTO state (key, value) VALUES ('initial_sync_finished', 'true')")
         .execute(&mut *conn)
--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,26 @@
+use serde::{Deserialize, Serialize};
+use serde_aux::prelude::deserialize_option_number_from_string;
+
 #[derive(sqlx::FromRow)]
 pub struct DBState {
     pub key: String,
     pub value: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, sqlx::FromRow)]
+pub struct Song {
+    #[serde(rename(deserialize = "_id"))]
+    pub id: String,
+    #[serde(rename(deserialize = "songURL"))]
+    pub external_url: Option<String>,
+    pub name: Option<String>,
+    #[serde(rename(deserialize = "songName"))]
+    pub song_name: String,
+    #[serde(rename(deserialize = "songID"))]
+    pub song_id: String,
+    #[serde(rename(deserialize = "downloadUrl"))]
+    pub download_url: String,
+    #[serde(default)]
+    #[serde(rename(deserialize = "levelID"), deserialize_with = "deserialize_option_number_from_string")]
+    pub level_id: Option<String>,
 }
